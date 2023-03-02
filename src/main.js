bca2aeff5ccdb457d239bd6900b3fa8b89772173
nina <kana202206@gmail.com>
2023-03-03 00:12:44 +0800

    git add

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import './assets/all.scss'
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
+app.use(VueAxios, axios)
